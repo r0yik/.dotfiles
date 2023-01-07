c108b97cc52dada7bda48518ba499d03643b4e96
royik <ryoheiikezawa@protonmail.com>
2023-01-07 14:52:46 +0900

    multiple monitor setup

--- .config/herbstluftwm/panel.sh
+++ .config/herbstluftwm/panel.sh
@@ -6,12 +6,7 @@
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c $HOME/.config/herbstluftwm/hlwm-polybar.conf hlwm-main &
-  done
-else
-  polybar --reload -c $HOME/.config/herbstluftwm/hlwm-polybar.conf hlwm-main &
-fi
+polybar --reload -c $HOME/.config/herbstluftwm/hlwm-polybar.conf hlwm-main &
+polybar --reload -c $HOME/.config/herbstluftwm/hlwm-polybar.conf hlwm-sub &
 
 echo "Bars launched..."
